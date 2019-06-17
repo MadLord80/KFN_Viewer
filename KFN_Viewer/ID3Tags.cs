35aee7b524f3af2acb81fc3bc31fe564bdd6f245
MadLord <madlord@list.ru>
2019-06-17 22:37:49 +0700

    update ID3Tags class

--- KFN_Viewer/ID3Tags.cs
+++ KFN_Viewer/ID3Tags.cs
@@ -1,14 +1,29 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.IO;
 
 namespace KFN_Viewer
 {
     class ID3Tags
     {
+        public byte[] ModTags(byte[] data)
+        {
+            Stream dataStream = new MemoryStream(data);
+            TagLib.File tagData = TagLib.File.Create(new FileBytesAbstraction("tempFile.mp3", dataStream));
+            //TagLib.Tag tempTags = new TagLib.Id3v2.Tag();
+            //tagData.Tag.Lyrics = "test lyric";
+            //tagData.RemoveTags(TagLib.TagTypes.AllTags);
+            //tagData.Tag.CopyTo(tempTags, true);
+            tagData.Save();
+            tagData.Dispose();
+            //tagData = TagLib.File.Create(new FileBytesAbstraction(audioFile, dataStream));
+            //tempTags.CopyTo(tagData.Tag, true);
+            //tagData.Save();
+            //tagData.Dispose();
+
+            byte[] dStream = new byte[dataStream.Length];
+            dataStream.Read(dStream, 0, dStream.Length);
+            return dStream;
+        }
+
         private class FileBytesAbstraction : TagLib.File.IFileAbstraction
         {
             public FileBytesAbstraction(string name, Stream data)
